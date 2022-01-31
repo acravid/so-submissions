8fe10e2e762eaf7cca5804e1321a98bfd2881637
alravid <allancravid@tecnico.ulisboa.pt>
2022-01-31 21:56:39 +0000

    refactor: client's API | tfs_read tfs_shutdown_after_all_closed

--- tecnicofs_ex2/client/tecnicofs_client_api.c
+++ tecnicofs_ex2/client/tecnicofs_client_api.c
@@ -76,12 +76,12 @@ int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     strcpy(buffer+1 , client_pipe_path);
 
     if(send_to_server(fserver,buffer, sizeof(char)*(MAX_PIPE_LEN + 1)) != sizeof(char)*(MAX_PIPE_LEN + 1)) {
-        perror("client in tfs_mount: error sending request to server");
+        perror("client in tfs_mount: error sending request to server\n");
         return -1;
     }
     
     if(receive_from_server(fclient,buffer,sizeof(int)) != sizeof(int)) {
-        perror("client in tfs_mount: error receiving from server");
+        perror("client in tfs_mount: error receiving from server\n");
         return -1;
     }
 
@@ -97,7 +97,7 @@ int tfs_unmount() {
     ((int*) ((char*)buffer)+1)[0] = session_id;
 
     if(send_to_server(fserver, buffer, sizeof(char) + sizeof(int)) != (sizeof(char) + sizeof(int))) {
-        perror("client in tfs_unmount: error sending request to server");
+        perror("client in tfs_unmount: error sending request to server\n");
         return -1;
     }
 
@@ -121,12 +121,12 @@ int tfs_open(char const *name, int flags) {
 
     
     if(send_to_server(buffer,fserver,(MAX_PIPE_LEN + 1) * sizeof(char) + 2 * sizeof(int)) != (MAX_PIPE_LEN + 1) * sizeof(char) + 2 * sizeof(int)) {
-        perror("client in tfs_open: error sending request to server ");
+        perror("client in tfs_open: error sending request to server\n");
         return -1;
     }
 
     if(receive_from_server(buffer,fclient,sizeof(int)) != sizeof(int)) {
-        perror("client in tfs_open: error receiving from server");
+        perror("client in tfs_open: error receiving from server\n");
         return -1;
     }
    
@@ -146,13 +146,13 @@ int tfs_close(int fhandle) {
     ((int*)last_pos)[0] = fhandle;
 
     if(send_to_server(fserver,buffer, sizeof(char) + 2 * sizeof(int)) != (sizeof(char) + 2 * sizeof(int))) {
-        perror("client in tfs_close: error sending request to server");
+        perror("client in tfs_close: error sending request to server\n");
         return -1;
 
     }
 
     if(receive_from_server(fclient,buffer,sizeof(int)) != (sizeof(int))) {
-        perror("client in tfs_close: error receiving from server");
+        perror("client in tfs_close: error receiving from server\n");
         return -1;
     }
     
@@ -173,64 +173,72 @@ ssize_t tfs_write(int fhandle, void const *buffer, size_t len) {
     last_pos = (void*)(((int*)last_pos) + 1);
     ((int*)last_pos)[0] = fhandle;
     last_pos = (void*)(((int*)last_pos) + 1);
-    ((size_t*)last_pos)[0] = fhandle;
+    ((size_t*)last_pos)[0] = fhandle;  
     last_pos = (void*)(((size_t*)last_pos) + 1);
     memcpy(command , buffer , len);
 
     if(send_to_server(fserver,command,(1 + len) * sizeof(char) + 3 * sizeof(int)) != ((1 + len) * sizeof(char) + 3 * sizeof(int))) {
-        perror("client in tfs_write: error sending request to server");
+        perror("client in tfs_write: error sending request to server\n");
         return -1;
 
     }
     size_t written_bytes = 0;
     if(written_bytes = receive_from_server(fclient,command, sizeof(int)) != sizeof(int)) { // should it be sizeof(size_t) here
-        perror("client in tfs_write: error receiving from the server");
+        perror("client in tfs_write: error receiving from the server\n");
         return -1;
     }
 
-    return written_bytes;
+    return (ssize_t)written_bytes;
 
 }
 
 ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
-    /* TODO: Implement this */
 
-    /* (char) OP_CODE=6 | (int) session_id | (int) fhandle | (size_t) len*/
+    void *buffer = malloc(sizeof(char) + sizeof(int)*3);
+    void *last_pos = buffer;
+    ((char*)last_pos)[0] = READ; 
+    last_pos = (void*)(((char*)last_pos) + 1);
+    ((int*)last_pos)[0] = session_id;
+    last_pos = (void*)(((int*)last_pos) + 1);
+    ((int*)last_pos)[0] = fhandle;
+    last_pos = (void*)(((int*)last_pos) + 1);
+    ((size_t*)last_pos)[0] = len;
+ 
 
-    void* buffer = malloc(sizeof(char) + sizeof(int) + sizeof(int) + sizeof(size_t));
-    ((char*)buffer)[0] = READ;
-    ((int*) ((char*)buffer)+ 1)[0] = session_id;
-    ((int*) ((char*)buffer)+ 1)[1] = fhandle;
-    ((size_t*)((int*)buffer) + 2)[0] = len;
+    if(send_to_server(fserver,buffer,sizeof(char) + 3 * sizeof(int)) != (sizeof(char) + 3 * sizeof(int))) {
+        perror("client in tfs_read: error sending request to server\n");
+        return -1;
+    }   
 
-    if(write(fserver,buffer,sizeof(char) + sizeof(int) + sizeof(int) + sizeof(size_t)) <= 0) {
+    void *answer_from_server = malloc(sizeof(int) + sizeof(char)*MAX_FILE_LENGTH);
+
+    if(receive_from_server(fclient,answer_from_server,sizeof(int) + sizeof(char)*MAX_FILE_LENGTH) < 0) {
+        perror("client in tfs_read: error receiving from the server\n");
         return -1;
     }
 
-    int bytes_read = read(fclient,buffer,sizeof(int)); /*+ sizeof(char)*/ 
+    // what's the return ? number_of_read_bytes | read content 
 
-    if(bytes_read <= 0) {
-        return -1;
-    } else {
-        return bytes_read;
-    }
+    return 0;
    
 }
 
 int tfs_shutdown_after_all_closed() {
-    /* TODO: Implement this */
 
     void* buffer = malloc(sizeof(char) + sizeof(int));
-    ((char*)buffer)[0] = SHUT_DOWN;
-    ((int*) ((char*)buffer)+1)[0] = session_id;
-
-
-    if(write(fserver ,buffer , sizeof(char) + sizeof(int)) <= 0) {
+    void *last_pos = buffer;
+    ((char*)last_pos)[0] = SHUT_DOWN;
+    last_pos = (void*)(((char*)last_pos) + 1);
+    ((int*)last_pos)[0] = session_id;
+    
+    if(send_to_server(fserver,buffer,sizeof(char) + sizeof(int)) != (sizeof(char) + sizeof(int))) {
+        perror("client in tfs_shutdown_after_all_closed: error sending request to server\n");
         return -1;
     }
 
-    if(read(fclient,buffer,sizeof(int)) <= 0)  { // <= ? < ?
-        return - 1;
+    if(receive_from_server(fclient,buffer,sizeof(int)) != sizeof(int)){
+        perror("client in tfs_shutdown_after_all_closed: error receiving from server");
+        return -1;
     }
 
     return 0;
